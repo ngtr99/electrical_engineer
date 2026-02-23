3fe9d98baee2c6db574b2119648bd40c349e3481
Nguyet Minh Trang <minhnguyet12923@gmail.com>
2026-02-23 09:34:53 -0800

    Switch database from SQLite to MongoDB

--- electrical_engineer/settings.py
+++ electrical_engineer/settings.py
@@ -82,10 +82,13 @@ WSGI_APPLICATION = 'electrical_engineer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+MONGO_URI = os.getenv("MONGO_URI")
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django_mongodb_engine',
+        'NAME': 'electrical_engineer',
+        'HOST': MONGO_URI,
     }
 }
 
