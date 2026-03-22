4dd947753ad5af799ebca593721b3578ff7ab1a3
ngtr99 <minhnguyet12923@gmail.com>
2026-03-22 01:28:51 -0700

    fix

--- electrical/models.py
+++ electrical/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-# Create your models here.
 class ElectricalData(models.Model):
     name = models.CharField(max_length=100)
     date = models.DateField()
     description = models.TextField()
-    picture = models.URLField(blank=True, null=True)
-    skills = models.TextField(blank = True, null = True)
+    picture_url = models.URLField(blank=True, null=True)
+    skills = models.TextField(blank=True, null=True)
+
     def __str__(self):
         return self.name
