146b2fd01f16f0aa22e0c10d8a89bc89cd62678c
Nguyet Minh Trang <minhnguyet12923@gmail.com>
2026-04-06 20:18:40 -0700

    Delete electrical directory

--- electrical/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import ElectricalData
-
-# Register your models here.
-admin.site.register(ElectricalData)
--- electrical/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ElectricalConfig(AppConfig):
-    name = 'electrical'
